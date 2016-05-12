6d2176532aadbdb3979c5a737c134181d48f7b7c
Marmann <saghrounihichem@hotmail.fr>
2016-05-12 10:47:51 +0200

    Ajout de maj_N pour avoir le décompte des stations

--- preliminaires.py
+++ preliminaires.py
@@ -1,8 +1,9 @@
 stations_coord = dict()
 stations_bornes = dict()
 stations_velos = dict()
+N = 0
 
-# ##
+##
 def maj_coord():
     stations = open('Paris.csv', 'r')
     stationreader = csv.reader(stations, delimiter = ',')
@@ -28,6 +29,16 @@ def maj_velos():
     s = r.json()
     for station in s:
         stations_velos[str(station['number'])] = station['available_bikes']
+
+##
+def maj_N():
+    url = 'https://api.jcdecaux.com/vls/v1/stations?contract=Paris&apiKey=08d9a2281bfac62f9b1ec531426ee7de8a7abfc0'
+    r = requests.get(url)
+    s = r.json()
+    global N
+    N = len(s)
+
+
 
 
 
